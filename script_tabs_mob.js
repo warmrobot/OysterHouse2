44d59d10b055d65923ec8ac984e167e0a9f95b04
YuliaLitvinenko <julia-litvinenko@yandex.ru>
2018-07-17 09:47:03 +0300

    Tabs mobile version + classes beautified

--- script_tabs_mob.js
+++ script_tabs_mob.js
@@ -2,8 +2,8 @@ function setup_for_width(mql) {
 	if (mql.matches) {
 		document.getElementById("tab1").innerText = '';
 		document.getElementById("tab2").innerText = '';
-		document.getElementById("tab3").innerText = '';
-		document.getElementById("tab4").innerText = '';
+		document.getElementById("tab3").innerHTML = '';
+		document.getElementById("tab4").innerHTML = '<img class="tab4" src="./images/crab.svg" alt="" height=80px width=80px/>';
 	} else {
 		document.getElementById("tab1").innerText = 'Хасанские устрицы';
 		document.getElementById("tab2").innerText = 'Императорские устрицы';
@@ -12,7 +12,7 @@ function setup_for_width(mql) {
 	}
 }
 
-var mql = window.matchMedia("screen and (max-width: 767px)");
+var mql = window.matchMedia("screen and (max-width: 800px)");
 
 mql.addListener(setup_for_width); 
 
